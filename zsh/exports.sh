a5e013d36e9a405c7e7c10970fdb0609ba741af5
kpxv <aden@sessionkey.net>
2025-12-06 20:43:35 -0500

    sync

---
+++ zsh/exports.sh
@@ -0,0 +1,9 @@
+# Convenience paths
+
+export MO2_CRASH_LOGS="/home/main/.steam/steam/steamapps/compatdata/489830/pfx/drive_c/users/steamuser/Documents/My Games/Skyrim Special Edition/SKSE/Crashlogs"
+export SSE_STOCK="/home/main/.local/share/Steam/steamapps/common/Skyrim Special Edition"
+export STEAM_CMPT="/home/main/.local/share/Steam/steamapps/compatdata"
+
+export VEX_SDK_PATH="/home/main/.config/Code\ -\ OSS/User/globalStorage/vexrobotics.vexcode/sdk/cpp/V5/V5_20240802_15_00_00"
+
+export QMK_KFT="/home/main/qmk_firmware/keyboards/cantor/keymaps/katarafett"
